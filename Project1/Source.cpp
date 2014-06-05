461e0d7fe287bac011fe8767ab837aa7bd660356
mmajidi <alimirmajidi@gmail.com>
2014-06-05 11:44:05 +0430

    test v1

---
+++ Project1/Source.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <windows.h>
+
+
+int main() 
+{
+
+typedef VOID (*DLLPROC) (int , int);
+
+HINSTANCE hinstDLL;
+DLLPROC Enumproc;
+BOOL fFreeDLL;
+
+
+hinstDLL = LoadLibrary("enumProcessDll.dll");
+
+    Enumproc = (DLLPROC) GetProcAddress(hinstDLL, "Enumproc");
+
+
+	Enumproc (200 , 20 );
+
+	fFreeDLL = FreeLibrary(hinstDLL);
+
+
+return 0;
+}
